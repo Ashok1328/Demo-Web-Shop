1f99c8511f552638f3c336e1bf098f3997e3400d
Ashok1328 <ashokkhatiwada1990@gmail.com>
2026-06-12 23:05:29 +0545

    Register Test

---
+++ src/tests/Register.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import { RegisterTestData } from "../data/TestData";
+import { RegisterPage } from "../page/RegisterPage";
+
+test.describe("Register Page", () => {
+  test("should allow user to register successfully", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.gotoRegisterPage();
+    await expect(page).toHaveURL(/register/);
+  });
+
+  test("Register with valid data", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    const data = new RegisterTestData();
+
+    await registerPage.gotoRegisterPage();
+    await registerPage.register(data);
+
+    // Verify a success message or result is visible
+    await expect(page.locator(".result")).toBeVisible();
+  });
+});
